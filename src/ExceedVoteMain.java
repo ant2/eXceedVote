4a435b965f58d9a64e66ae46036976bc48098c5b
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-09-13 11:19:17 +0700

    add some test code so we can experiment

--- src/ExceedVoteMain.java
+++ src/ExceedVoteMain.java
@@ -10,6 +10,14 @@ public class ExceedVoteMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("Welcome to eXceed Vote");
+		System.out.println("-----------------");
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
+		System.out.println("Test 4");
+		System.out.println("Test 5");
+		System.out.println("Test 6");
 	}
 
 }
