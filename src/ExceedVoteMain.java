e3c44db8ce35a8d25f62ded1c8398759c414b2eb
Artima <atm6415@gmail.com>
2012-09-13 14:23:06 +0700

    add test999

--- src/ExceedVoteMain.java
+++ src/ExceedVoteMain.java
@@ -18,7 +18,7 @@ public class ExceedVoteMain {
 		System.out.println("Test 5");
 		System.out.println("Test 6");
 		System.out.println("Test 8");
-		System.out.println("Test 9999");
+		System.out.println("Test 9");
+		System.out.println("Test 999");
 	}
-
 }
