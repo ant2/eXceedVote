a70429005bbe1ac021a813251d10de691e726c79
Thiwat Rongsirigul <thiwat9@hotmail.com>
2012-09-27 10:18:13 +0700

    Create class VoterProfile and Voter

--- src/com/github/ant2/exceedvote/Voter.java
+++ src/com/github/ant2/exceedvote/Voter.java
@@ -1,15 +1,22 @@
 package com.github.ant2.exceedvote;
 
 public class Voter {
-
+	private VoterProfile profile;
+	
+	public Voter() {
+		this(new VoterProfile("Test User", "5410000000"));
+	}
+	
+	public Voter(VoterProfile profile) {
+		this.profile = profile;
+	}
+	
 	public int getAllowedBallots() {
-		// TODO Auto-generated method stub
 		return 5;
 	}
 
 	public VoterProfile getProfile() {
-		// TODO Auto-generated method stub
-		return null;
+		return profile;
 	}
 
 }
