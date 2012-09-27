c4dcab63f9043a26cd9e3ca90e46b633a36f8a93
natsu yasumi <natsu_x3@hotmail.com>
2012-09-27 10:46:45 +0700

    add javadoc of BallotBox

--- src/com/github/ant2/exceedvote/BallotBox.java
+++ src/com/github/ant2/exceedvote/BallotBox.java
@@ -3,23 +3,47 @@ package com.github.ant2.exceedvote;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A BallotBox that keeps a list of ballots.
+ * 
+ * @author Nutnicha Charoenporn
+ */
 public class BallotBox {
-	
+
+	/** A list of ballots */
 	private List<Ballot> list;
-	
-	public BallotBox(){
+
+	/**
+	 * Constructs a list of ballots.
+	 */
+	public BallotBox() {
 		list = new ArrayList<Ballot>();
 	}
 
-	public void add(Ballot ballot){
+	/**
+	 * Adds ballot into the list.
+	 * 
+	 * @param ballot that added
+	 */
+	public void add(Ballot ballot) {
 		list.add(ballot);
 	}
-	
-	public List<Ballot> getVoterBallots(){
+
+	/**
+	 * Gets the list of ballots.
+	 * 
+	 * @return list of ballots
+	 */
+	public List<Ballot> getVoterBallots() {
 		return list;
 	}
-	
-	public int countBallot(){
+
+	/**
+	 * Counts ballots in the list.
+	 * 
+	 * @return number of ballots in the list
+	 */
+	public int countBallot() {
 		return list.size();
 	}
 }
