4fb4a3d23e0b18c61d4ece827a3326b05eb6b3df
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-09-27 09:47:49 +0700

    add a test method

--- src/com/github/ant2/exceedvote/ExceedVoteMain.java
+++ src/com/github/ant2/exceedvote/ExceedVoteMain.java
@@ -10,19 +10,11 @@ public class ExceedVoteMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Welcome to eXceed Vote Program");
-		System.out.println("==============================");
-		System.out.println("Test 1");
-		System.out.println("Test 2");
-		System.out.println("Test 3");
-		System.out.println("Test 4");
-		System.out.println("Test 5");
-		System.out.println("Test 6");
-		System.out.println("Test 7");
-		System.out.println("Test 8");
-		System.out.println("Test 9");
-		System.out.println("Test 999");
-		System.out.println("Test 10");
-		System.out.println("TESTTTT 1000");
+		test();
 	}
+
+	private static void test() {
+	}
+	
+	
 }
