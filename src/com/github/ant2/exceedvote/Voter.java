f867b185eff3daf89c95ea149da466905e9b21aa
Thiwat Rongsirigul <thiwat9@hotmail.com>
2012-09-27 10:50:20 +0700

    add javadoc for the VoterProfile and Voter classes

--- src/com/github/ant2/exceedvote/Voter.java
+++ src/com/github/ant2/exceedvote/Voter.java
@@ -1,20 +1,46 @@
 package com.github.ant2.exceedvote;
 
+/**
+ * The Voter that represents a person that can vote.
+ * 
+ * @author Thiwat Rongsirigul
+ */
 public class Voter {
+	/** Profile for identify who is this voter */
 	private VoterProfile profile;
-	
+
+	/**
+	 * Test constructor.
+	 */
 	public Voter() {
 		this(new VoterProfile("Test User", "5410000000"));
 	}
-	
+
+	/**
+	 * Constructs the voter from the specific profile.
+	 * 
+	 * @param profile
+	 *            the identification detail for this voter
+	 */
 	public Voter(VoterProfile profile) {
 		this.profile = profile;
 	}
-	
+
+	/**
+	 * Returns the allowed ballot for this user which is the total times that
+	 * this user can vote.
+	 * 
+	 * @return total ballot that commissioner assigned to this voter
+	 */
 	public int getAllowedBallots() {
 		return 5;
 	}
 
+	/**
+	 * Returns the information of this voter.
+	 * 
+	 * @return profile object that describe this voter
+	 */
 	public VoterProfile getProfile() {
 		return profile;
 	}
