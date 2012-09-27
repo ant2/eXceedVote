96e67fa966855e029debdd138f7efa68c03f3eb0
Artima <atm6415@gmail.com>
2012-09-27 10:17:57 +0700

    implement Project class and Criterion class

--- src/com/github/ant2/exceedvote/Project.java
+++ src/com/github/ant2/exceedvote/Project.java
@@ -1,5 +1,26 @@
 package com.github.ant2.exceedvote;
 
 public class Project {
-
+	private String name;
+	private String description;
+	public Project(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+	
+	public String getName() { 
+		return name; 
+	}
+	
+	public String description() { 
+		return description; 
+	}
+	
+	public void setName(String name) { 
+		this.name = name; 
+	}
+	
+	public void setDescription(String description) { 
+		this.description = description; 
+	}
 }
