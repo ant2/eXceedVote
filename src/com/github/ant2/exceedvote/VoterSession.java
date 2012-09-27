1fd0ae71f0dcd75752ef750613b04939e08420be
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-09-27 10:14:23 +0700

    implement class VoterSession

--- src/com/github/ant2/exceedvote/VoterSession.java
+++ src/com/github/ant2/exceedvote/VoterSession.java
@@ -1,27 +1,43 @@
 package com.github.ant2.exceedvote;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class VoterSession {
+	
+	private Calendar startTime;
+	private Calendar finishTime;
+	private Calendar announcementTime;
+	
+	private BallotBox ballotBox;
+	
+	public VoterSession() {
+		ballotBox = new BallotBox();
+		
+		// TODO put actual data here!
+		startTime = Calendar.getInstance();
+		finishTime = Calendar.getInstance();
+		finishTime.add(Calendar.HOUR_OF_DAY, 1);
+	}
 
 	public BallotBox getBallotBox() {
-		// TODO Auto-generated method stub
-		return null;
+		return ballotBox;
 	}
 
 	public int getRemainingTime() {
-		// TODO Auto-generated method stub
-		return 300;
+		return (int)((finishTime.getTimeInMillis() -
+				Calendar.getInstance().getTimeInMillis()) / 1000)
 	}
 
 	public List<Project> getProjects() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Project> list = new ArrayList<Project>();
+		return list;
 	}
 
 	public List<Criterion> getCriteria() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Criterion> list = new ArrayList<Criterion>();
+		return list;
 	}
 
 }
