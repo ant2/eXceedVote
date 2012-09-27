4b2633b54f5bf5c22577e05514b600294d4138b9
Artima <atm6415@gmail.com>
2012-09-27 10:46:27 +0700

    add javadoc to Project and Criterion

--- src/com/github/ant2/exceedvote/Criterion.java
+++ src/com/github/ant2/exceedvote/Criterion.java
@@ -1,16 +1,43 @@
 package com.github.ant2.exceedvote;
 
+/**
+ * A criterion which voter can vote for.
+ * 
+ * @author Artima Mahahemarat
+ */
 public class Criterion {
+	/** criterion name */
 	private String name;
+	
+	/**
+	 * Constructs a criterion with name.
+	 * 
+	 * @param name
+	 */
 	public Criterion(String name) { 
 		this.name = name; 
 	}
 	
+	/**
+	 * Returns criterion name.
+	 * 
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Sets criterion name.
+	 * 
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
 	}
 }
--- src/com/github/ant2/exceedvote/Project.java
+++ src/com/github/ant2/exceedvote/Project.java
@@ -1,26 +1,65 @@
 package com.github.ant2.exceedvote;
 
+/**
+ * A project which voter can vote for.
+ * 
+ * @author Artima Mahahemarat
+ */
 public class Project {
+	/** project name */
 	private String name;
+	/** project description */
 	private String description;
+
+	/**
+	 * Constructs a project with name and description.
+	 * 
+	 * @param name
+	 * @param description
+	 */
 	public Project(String name, String description) {
 		this.name = name;
 		this.description = description;
 	}
-	
-	public String getName() { 
-		return name; 
+
+	/**
+	 * Returns project name.
+	 * 
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns project description.
+	 * 
+	 * @return description
+	 */
+	public String description() {
+		return description;
+	}
+
+	/**
+	 * Sets project name.
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Sets project description.
+	 * 
+	 * @param description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
 	}
 	
-	public String description() { 
-		return description; 
-	}
-	
-	public void setName(String name) { 
-		this.name = name; 
-	}
-	
-	public void setDescription(String description) { 
-		this.description = description; 
+	@Override
+	public String toString() { 
+		return name;
 	}
 }
