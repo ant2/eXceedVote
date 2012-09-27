419d2072cb58d5e26873e6f93c36ceca2afae226
Thiwat Rongsirigul <thiwat9@hotmail.com>
2012-09-27 11:33:25 +0700

    implement the Ballot class with javadoc

--- src/com/github/ant2/exceedvote/Ballot.java
+++ src/com/github/ant2/exceedvote/Ballot.java
@@ -1,14 +1,39 @@
 package com.github.ant2.exceedvote;
 
+import java.util.Calendar;
+
+/**
+ * The Ballot vote for the specific project on a specific criterion.
+ *
+ * @author Thiwat Rongsirigul (Leo Aiolia)
+ */
 public class Ballot {
+	private Calendar votedAt;
+	private Voter voter;
+	private Criterion criterion;
 
+	/**
+	 * Constructs the Ballot with the specific criteria and for the Voter.
+	 * 
+	 * @param v
+	 *            the owner of this Ballot
+	 * @param selectedProject
+	 *            the project that this Ballot voted for
+	 * @param selectedCriteria
+	 *            the criteria that this Ballot voted for
+	 */
 	public Ballot(Voter v, Project selectedProject, Criterion selectedCriteria) {
-		// TODO Auto-generated constructor stub
+		votedAt = Calendar.getInstance();
+		this.voter = v;
+		this.criterion = selectedCriteria;
 	}
 
+	/**
+	 * Returns the Voter that owns this Ballot.
+	 * 
+	 * @return the owner of this Ballot
+	 */
 	public Voter getVoter() {
-		// TODO Auto-generated method stub
-		return null;
+		return voter;
 	}
-
 }
