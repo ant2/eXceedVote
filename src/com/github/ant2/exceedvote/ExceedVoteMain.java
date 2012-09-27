ff24f7c8ff22674cdc3ba943eb4ea3aa7a0798e7
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-09-27 11:23:55 +0700

    format

--- src/com/github/ant2/exceedvote/ExceedVoteMain.java
+++ src/com/github/ant2/exceedvote/ExceedVoteMain.java
@@ -2,7 +2,6 @@ package com.github.ant2.exceedvote;
 
 import java.util.List;
 
-
 /**
  * eXceed Vote Main Class
  * 
@@ -18,42 +17,43 @@ public class ExceedVoteMain {
 	}
 
 	private static void test() {
-		
+
 		Voter v = new Voter();
 		VoterSession session = new VoterSession();
-		
+
 		int allowedBallots = v.getAllowedBallots();
 		System.out.println("Allowed ballots " + allowedBallots);
-		
+
 		VoterProfile profile = v.getProfile();
 		BallotBox box = session.getBallotBox();
-		
+
 		System.out.println("Hello, " + profile);
-		
+
 		int remainingTime = session.getRemainingTime();
-		System.out.printf("We have %d : %d left\n", remainingTime / 60, remainingTime % 60);
-		
+		System.out.printf("We have %d : %d left\n", remainingTime / 60,
+				remainingTime % 60);
+
 		List<Project> projects = session.getProjects();
 		List<Criterion> criteria = session.getCriteria();
-		
+
 		System.out.println("Projects:");
 		for (Project project : projects) {
 			System.out.println("- " + project);
 		}
-		
+
 		System.out.println("Criteria:");
 		for (Criterion criterion : criteria) {
 			System.out.println("- " + criterion);
 		}
-		
+
 		// user selects a project
 		Project selectedProject = projects.get(0);
 		Criterion selectedCriteria = criteria.get(0);
-		
+
 		// use that information to create a ballot
 		Ballot b = new Ballot(v, selectedProject, selectedCriteria);
 		session.submit(b);
-		
+
 	}
-	
+
 }
