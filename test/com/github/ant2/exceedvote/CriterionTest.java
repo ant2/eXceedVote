d6224193e61c921698622534de4a289b1f2eeb46
Artima <atm6415@gmail.com>
2012-10-01 23:43:47 +0700

    Add JUnit test for Criterion class

---
+++ test/com/github/ant2/exceedvote/CriterionTest.java
@@ -0,0 +1,31 @@
+package com.github.ant2.exceedvote;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for Criterion class.
+ *
+ * @author Artima Mahahemarat
+ */
+public class CriterionTest {
+	private Criterion criterion = new Criterion("Test");
+	
+	@Test
+	public void testGetName() {
+		assertEquals("Test", criterion.getName());
+	}
+	
+	@Test
+	public void testSetName() {
+		criterion.setName("New Test");
+		assertEquals("New Test", criterion.getName());
+	}
+
+	@Test
+	public void testToString() {
+		assertEquals("Test", criterion.toString());
+	}
+	
+}
