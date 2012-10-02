53b7a6969261eeb0ee7f54bf8c78cdee74ba45aa
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-10-02 16:20:15 +0700

    implement first part of ballotcontroller

--- src/com/github/ant2/exceedvote/controller/BallotController.java
+++ src/com/github/ant2/exceedvote/controller/BallotController.java
@@ -1,11 +1,15 @@
 package com.github.ant2.exceedvote.controller;
 
+import org.apache.log4j.Logger;
+
 import com.github.ant2.exceedvote.model.Ballot;
 import com.github.ant2.exceedvote.model.VotingProcess;
 import com.github.ant2.exceedvote.view.BallotView;
+import com.github.ant2.exceedvote.view.BallotView.Delegate;
 
-public class BallotController {
+public class BallotController implements Delegate {
 
+	private Logger logger = Logger.getLogger(BallotController.class);
 	private VotingProcess process;
 	private Ballot model;
 	private BallotView view;
@@ -14,11 +18,19 @@ public class BallotController {
 		this.process = process;
 		this.model = model;
 		this.view = view;
+		view.setDelegate(this);
 	}
 
 	public void show() {
 		view.pack();
 		view.setVisible(true);
+	}
+
+	@Override
+	public void submitted() {
+		logger.info("submit button was clicked");
+		view.setVisible(false);
+		view.dispose();
 	}
 
 }
--- src/com/github/ant2/exceedvote/view/BallotView.java
+++ src/com/github/ant2/exceedvote/view/BallotView.java
@@ -1,17 +1,68 @@
 package com.github.ant2.exceedvote.view;
 
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class BallotView extends JDialog {
+
+	public interface Delegate {
+		void submitted();
+	}
 
 	/** */
 	private static final long serialVersionUID = 1L;
 
+	private Delegate delegate;
+
 	public BallotView(Frame parent) {
 		super(parent, true);
 		setTitle("Vote for Project");
+		initComponents();
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+
+	public void setDelegate(Delegate delegate) {
+		this.delegate = delegate;
+	}
+
+	private void initComponents() {
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+		JPanel projectsPanel = new JPanel();
+		JPanel criteriaPanel = new JPanel();
+
+		projectsPanel.setBorder(BorderFactory
+				.createTitledBorder(" Select Project "));
+		criteriaPanel.setBorder(BorderFactory
+				.createTitledBorder(" Pick a Criterion "));
+
+		JButton submitButton = new JButton(new AbstractAction("Submit") {
+			/** */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int result = JOptionPane.showConfirmDialog(BallotView.this,
+						"Are you sure?", "eXceed Vote Confirmation",
+						JOptionPane.YES_NO_OPTION);
+				if (result == JOptionPane.YES_OPTION) {
+					delegate.submitted();
+				}
+			}
+		});
+
+		add(projectsPanel);
+		add(criteriaPanel);
+		add(submitButton);
+
 	}
 
 }
