93e62689c60f00692749a8690115054d36408807
natsu yasumi <natsu_x3@hotmail.com>
2012-10-31 21:08:51 +0700

    add ViewProjectProcess Class, finished a javadoc, but still has some problems with an event ;w;

--- src/com/github/ant2/exceedvote/model/process/ViewProjectProcess.java
+++ src/com/github/ant2/exceedvote/model/process/ViewProjectProcess.java
@@ -1,24 +1,57 @@
 package com.github.ant2.exceedvote.model.process;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.github.ant2.exceedvote.model.Project;
 import com.github.ant2.exceedvote.model.VoteEvent;
 
+/**
+ * A view of the project's process.
+ * 
+ * @author Nutnicha Charoenporn
+ */
 public class ViewProjectProcess {
 
+	/** A list of projects */
+	private List<Project> list;
+	
+	/** A vote event */
+	private VoteEvent event;
+
+	/**
+	 * Constructs the ViewProjectProcess. Initializes the event.
+	 */
 	public ViewProjectProcess(VoteEvent event) {
-		// TODO Auto-generated constructor stub
+		this.event = event;
+		list = event.getProjects();
 	}
 
+	/**
+	 * Gets the list of available projects.
+	 * 
+	 * @return a list of available projects.
+	 */
 	public List<Project> getAllAvailableProjects() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Project> projects = new ArrayList<Project>();
+		
+		for(Project project : list){
+			if(event.getProjects().contains(project)){
+				projects.add(project);
+			}
+		}
+		
+		return projects;
 	}
 
+	/**
+	 * Gets the project description
+	 * 
+	 * @param p1 the project
+	 * @return the project description
+	 */
 	public Object getProjectInfo(Project p1) {
-		// TODO Auto-generated method stub
-		return null;
+		return p1.getDescription();
 	}
 
 }
