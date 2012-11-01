f03f93f75ff917c141eb50c1270939b6f473de32
Artima <atm6415@gmail.com>
2012-11-01 09:23:19 +0700

    implement VotingProcess Class

--- src/com/github/ant2/exceedvote/model/process/VotingProcess.java
+++ src/com/github/ant2/exceedvote/model/process/VotingProcess.java
@@ -1,51 +1,71 @@
 package com.github.ant2.exceedvote.model.process;
-
 import java.util.List;
-
+import com.github.ant2.exceedvote.model.Ballot;
 import com.github.ant2.exceedvote.model.Criterion;
 import com.github.ant2.exceedvote.model.Project;
 import com.github.ant2.exceedvote.model.VoteEvent;
 import com.github.ant2.exceedvote.model.Voter;
+/**
+ * 
+ *
+ * @author Artima Mahahemarat
+ */
 
 public class VotingProcess {
-
+	private VoteEvent event;
+	private Voter voter;
+	private Criterion criterion;
+	private int count;
+	private int[] a;
+	
 	public VotingProcess(VoteEvent event, Voter voter, Criterion criterion) {
-		// TODO Auto-generated constructor stub
+		this.event = event;
+		this.voter = voter;
+		this.criterion = criterion;
+		a = new int[getProjects().size()];
 	}
 
 	public List<Project> getProjects() {
-		// TODO Auto-generated method stub
-		return null;
+		return event.getProjects();
 	}
 
 	public boolean canIncrease() {
-		// TODO Auto-generated method stub
-		return false;
+		return count < voter.getAllowedBallots();
 	}
 
 	public boolean canDecrease(Project project) {
-		// TODO Auto-generated method stub
-		return false;
+		int i = getProjects().indexOf(project);
+		return a[i] > 0;
 	}
 
 	public void increase(Project project) {
-		// TODO Auto-generated method stub
-
+		if (!canIncrease()) return;
+		int i = getProjects().indexOf(project);
+		a[i]++;
+		count++;
 	}
 
-	public Object getCount(Project project) {
-		// TODO Auto-generated method stub
-		return null;
+	public int getCount(Project project) {
+		int i = getProjects().indexOf(project);
+		return a[i];
 	}
 
 	public void decrease(Project project) {
-		// TODO Auto-generated method stub
-
+		if (!canDecrease(project)) return;
+		int i = getProjects().indexOf(project);
+		a[i]--;
+		count--;
 	}
 
 	public void submit() {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == 0) continue;
+			for (int j = 0; j < a[i]; j++) {
+				Project project = getProjects().get(i);
+				Ballot b = new Ballot(voter, project, criterion);
+				event.submit(b);
+			}
+		}
 	}
 
 }
