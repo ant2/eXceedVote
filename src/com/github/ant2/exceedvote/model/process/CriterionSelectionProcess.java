70cb5df580ce733ffb879890d723d5e63b84eade
natsu yasumi <natsu_x3@hotmail.com>
2012-11-15 10:36:03 +0700

    Add log4j for CriterionSelectionProcess

--- src/com/github/ant2/exceedvote/model/process/CriterionSelectionProcess.java
+++ src/com/github/ant2/exceedvote/model/process/CriterionSelectionProcess.java
@@ -2,16 +2,24 @@ package com.github.ant2.exceedvote.model.process;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.impl.Log4jContextFactory;
+
 import com.github.ant2.exceedvote.dao.DaoFactory;
 import com.github.ant2.exceedvote.model.domain.Criterion;
 import com.github.ant2.exceedvote.model.domain.VoteEvent;
 
 public class CriterionSelectionProcess {
 
+	private Logger logger = LogManager.getLogger(CriterionSelectionProcess.class);
+	
 	private VoteEvent event;
 	private Context context;
 
 	private DaoFactory df;
+
+	private List<Criterion> criteria;
 
 	public CriterionSelectionProcess(Context context) {
 		this.context = context;
@@ -20,7 +28,13 @@ public class CriterionSelectionProcess {
 	}
 
 	public List<Criterion> getAllCriteria() {
-		return df.getCriterionDao().findAllByEvent(event);
+		if (criteria == null) {
+			logger.debug("Getting all criteria.");
+			criteria = df.getCriterionDao().findAllByEvent(event);
+		} else {
+			logger.debug("Already got all criteria.");
+		}
+		return criteria;
 	}
 
 	public VotingProcess createVotingProcess(Criterion criterion) {
