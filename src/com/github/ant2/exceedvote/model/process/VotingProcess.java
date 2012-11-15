784e38e31df77f95931e842bbaf871fe34e6b81e
Artima <atm6415@gmail.com>
2012-11-15 11:41:22 +0700

    Add log4j for VotingProcess

--- src/com/github/ant2/exceedvote/model/process/VotingProcess.java
+++ src/com/github/ant2/exceedvote/model/process/VotingProcess.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.github.ant2.exceedvote.dao.DaoFactory;
 import com.github.ant2.exceedvote.model.domain.Criterion;
 import com.github.ant2.exceedvote.model.domain.Project;
@@ -16,11 +19,12 @@ import com.github.ant2.exceedvote.model.domain.Voter;
  * @author Artima Mahahemarat
  */
 public class VotingProcess {
-
+	
+	private Logger logger = LogManager.getLogger(VotingProcess.class);
 	private VoteEvent event;
 	private Voter voter;
 	private Criterion criterion;
-	private int count;
+	private int usedBallots;
 	private int[] a;
 	private DaoFactory df;
 	private List<Project> projects;
@@ -33,43 +37,65 @@ public class VotingProcess {
 		a = new int[getProjects().size()];
 	}
 
+	
 	public void setDaoFactory(DaoFactory df) {
+		logger.debug("Setting new DaoFactory.");
 		this.df = df;
 	}
 
 	public List<Project> getProjects() {
 		if (projects == null) {
+			logger.debug("Getting all projects.");
 			projects = df.getProjectDao().findAllByEvent(event);
+		} else {
+			logger.debug("Already got all projects.");
 		}
 		return projects;
 	}
 
 	public boolean canIncrease() {
-		return count < voter.getAllowedBallots();
+		logger.debug("Determine if used ballots are less than allowed.");
+		return usedBallots < voter.getAllowedBallots();
 	}
 
 	public boolean canDecrease(Project project) {
+		logger.debug("Getting index of project.");
 		int i = getProjects().indexOf(project);
+		logger.debug("Determine if project has ever been voted.");
 		return a[i] > 0;
 	}
 
 	public void increase(Project project) {
-		if (!canIncrease()) return;
+		if (!canIncrease()) {
+			logger.debug("Project cannot be increased!");
+			return;
+		}
+		logger.debug("Project can be increased.");
+		logger.debug("Getting index of project.");
 		int i = getProjects().indexOf(project);
+		logger.debug("Adding project count and used ballots.");
 		a[i]++;
-		count++;
+		usedBallots++;
 	}
 
 	public int getCount(Project project) {
+		logger.debug("Getting index of project.");
 		int i = getProjects().indexOf(project);
+		logger.debug("Retrieving project count.");
 		return a[i];
 	}
 
 	public void decrease(Project project) {
-		if (!canDecrease(project)) return;
+		if (!canDecrease(project)) {
+			logger.debug("Project cannot be decreased!");
+			return;
+		}
+		logger.debug("Project can be decreased.");
+		logger.debug("Getting index of project.");
 		int i = getProjects().indexOf(project);
+		logger.debug("Reducing project count and used ballots.");
 		a[i]--;
-		count--;
+		usedBallots--;
 	}
 
 	public void submit() {
@@ -87,14 +113,17 @@ public class VotingProcess {
 	}
 
 	public Criterion getCriterion() {
+		logger.debug("Retrieving criterion.");
 		return criterion;
 	}
 
 	public int getUsedBallots() {
-		return count;
+		logger.debug("Retrieving used ballots.");
+		return usedBallots;
 	}
 
 	public int getTotalBallots() {
+		logger.debug("Retrieving allowed ballots.");
 		return voter.getAllowedBallots();
 	}
 
