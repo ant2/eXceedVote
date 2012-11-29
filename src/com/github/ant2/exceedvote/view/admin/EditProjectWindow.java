1b20b3c1e5519cfcb990f56f700612093de87117
natsu yasumi <natsu_x3@hotmail.com>
2012-11-29 11:27:27 +0700

    Add EditProJectWindow

---
+++ src/com/github/ant2/exceedvote/view/admin/EditProjectWindow.java
@@ -0,0 +1,121 @@
+package com.github.ant2.exceedvote.view.admin;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+/**
+ * Editing projects UI.
+ * 
+ * @author Nutnicha Charoenporn
+ */
+
+public class EditProjectWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtProjectName;
+	private JPanel panel;
+	private JButton btnDelete;
+	private JButton btnSave;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EditProjectWindow frame = new EditProjectWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public EditProjectWindow() {
+		setTitle("Edit Project");
+		setResizable(false);
+		setForeground(Color.WHITE);
+		setBackground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),}));
+		
+		JLabel lblProjectName = new JLabel("Project Name : ");
+		lblProjectName.setBackground(Color.WHITE);
+		lblProjectName.setFont(new Font("Tahoma", Font.BOLD, 18));
+		contentPane.add(lblProjectName, "6, 8, right, default");
+		
+		txtProjectName = new JTextField();
+		txtProjectName.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		txtProjectName.setText("project name");
+		contentPane.add(txtProjectName, "8, 8, fill, default");
+		txtProjectName.setColumns(10);
+		
+		panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		contentPane.add(panel, "6, 14, 3, 1, fill, fill");
+		
+		btnDelete = new JButton("Delete");
+		btnDelete.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		panel.add(btnDelete);
+		
+		btnSave = new JButton("Save");
+		btnSave.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		panel.add(btnSave);
+	}
+
+}
