8b13bfdf021f508e2db5a794c0eb32efffb835a1
natsu yasumi <natsu_x3@hotmail.com>
2012-11-29 11:35:06 +0700

    Add EditCriteriaWindow

---
+++ src/com/github/ant2/exceedvote/view/admin/EditCriteriaWindow.java
@@ -0,0 +1,117 @@
+package com.github.ant2.exceedvote.view.admin;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+/**
+ * Editing criteria UI.
+ * 
+ * @author Nutnicha Charoenporn
+ */
+
+public class EditCriteriaWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtCriteriaName;
+	private JPanel panel;
+	private JButton btnDelete;
+	private JButton btnSave;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EditCriteriaWindow frame = new EditCriteriaWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public EditCriteriaWindow() {
+		setTitle("Edit Criteria");
+		setForeground(Color.WHITE);
+		setResizable(false);
+		setBackground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),}));
+		
+		JLabel lblCriteriaName = new JLabel("Criteria Name : ");
+		lblCriteriaName.setBackground(Color.WHITE);
+		lblCriteriaName.setFont(new Font("Tahoma", Font.BOLD, 18));
+		contentPane.add(lblCriteriaName, "6, 8, right, default");
+		
+		txtCriteriaName = new JTextField();
+		txtCriteriaName.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		txtCriteriaName.setText("criteria name");
+		contentPane.add(txtCriteriaName, "8, 8, fill, default");
+		txtCriteriaName.setColumns(10);
+		
+		panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		contentPane.add(panel, "6, 14, 3, 1, fill, fill");
+		
+		btnDelete = new JButton("Delete");
+		panel.add(btnDelete);
+		
+		btnSave = new JButton("Save");
+		panel.add(btnSave);
+	}
+
+}
