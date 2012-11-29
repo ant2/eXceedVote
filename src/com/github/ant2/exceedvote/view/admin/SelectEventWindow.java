d0e6b8ecf31bdd0671c689a605294d16ac773804
natsu yasumi <natsu_x3@hotmail.com>
2012-11-29 12:03:06 +0700

    Add SelectEventWindow

--- src/com/github/ant2/exceedvote/view/admin/SelectEventWindow.java
+++ src/com/github/ant2/exceedvote/view/admin/SelectEventWindow.java
@@ -1,12 +1,26 @@
 package com.github.ant2.exceedvote.view.admin;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import javax.swing.JLabel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.Font;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JButton;
+
+/**
+ * Select event UI.
+ * 
+ * @author Nutnicha Charoenporn
+ */
 
 public class SelectEventWindow extends JFrame {
 
@@ -41,8 +55,66 @@ public class SelectEventWindow extends JFrame {
 		contentPane.setForeground(Color.WHITE);
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+				ColumnSpec.decode("284px:grow"),},
+			new RowSpec[] {
+				FormFactory.LINE_GAP_ROWSPEC,
+				RowSpec.decode("14px"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),}));
+		
+		JLabel lblPleaseSelectEvent = new JLabel("Please select event to manage");
+		lblPleaseSelectEvent.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblPleaseSelectEvent.setBackground(Color.WHITE);
+		contentPane.add(lblPleaseSelectEvent, "2, 6");
+		
+		JList list = new JList();
+		list.setValueIsAdjusting(true);
+		list.setVisibleRowCount(5);
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"JList", "JList", "JList", "JList", "JList", "JList"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		list.setSelectedIndex(0);
+		list.setBackground(Color.WHITE);
+		contentPane.add(list, "2, 12, fill, fill");
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		contentPane.add(panel, "2, 20, fill, fill");
+		
+		JButton btnNewEvent = new JButton("New Event");
+		panel.add(btnNewEvent);
+		
+		JButton btnOk = new JButton("OK");
+		panel.add(btnOk);
 	}
 
 }
