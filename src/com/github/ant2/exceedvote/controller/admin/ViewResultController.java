1dae666ff6f356d7b6e787d688505a7d2aa6c8ed
natsu yasumi <natsu_x3@hotmail.com>
2012-12-18 14:31:46 +0700

    Add Javadoc for controller.admin

--- src/com/github/ant2/exceedvote/controller/admin/ViewResultController.java
+++ src/com/github/ant2/exceedvote/controller/admin/ViewResultController.java
@@ -1,8 +1,5 @@
 package com.github.ant2.exceedvote.controller.admin;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.github.ant2.exceedvote.model.domain.Criterion;
 import com.github.ant2.exceedvote.model.domain.Project;
 import com.github.ant2.exceedvote.model.process.admin.BallotCount;
@@ -10,14 +7,22 @@ import com.github.ant2.exceedvote.model.process.admin.BallotCounterResult;
 import com.github.ant2.exceedvote.model.process.admin.ViewResultProcess;
 import com.github.ant2.exceedvote.view.admin.ViewResultWindow;
 
+/**
+ * A controller for ViewResultProcess
+ * 
+ * @author Thai Pangsakulyanont
+ */
 public class ViewResultController {
 
-	private static Logger logger = LogManager
-			.getLogger(ViewResultController.class);
-	
 	private ViewResultProcess process;
 	private ViewResultWindow view;
 
+	/**
+	 * @param process
+	 *            process to control
+	 * @param view
+	 *            view to control
+	 */
 	public ViewResultController(ViewResultProcess process, ViewResultWindow view) {
 		this.process = process;
 		this.view = view;
@@ -40,10 +45,8 @@ public class ViewResultController {
 			BallotCount count = result.forCriterion(c);
 			html.append("<h2>" + c + "</h2>");
 			html.append("<table>");
-			html.append("<tr>")
-			.append("<th>Project Name</th>")
-			.append("<th>Votes</th>")
-			.append("</tr>");
+			html.append("<tr>").append("<th>Project Name</th>")
+					.append("<th>Votes</th>").append("</tr>");
 			for (Project p : process.getProjects()) {
 				html.append("<tr>").append("<td>" + p + "</td>")
 						.append("<td>" + count.get(p) + "</td>");
@@ -55,6 +58,9 @@ public class ViewResultController {
 		view.setText(html.toString());
 	}
 
+	/**
+	 * Runs the controller: display the window.
+	 */
 	public void run() {
 		view.setVisible(true);
 	}
