3a365f262a26d55eb87da43b743b53bc7c7904e2
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-12-18 14:38:57 +0700

    add javadoc

--- src/com/github/ant2/exceedvote/view/VerticallyCenteredPanel.java
+++ src/com/github/ant2/exceedvote/view/VerticallyCenteredPanel.java
@@ -5,11 +5,22 @@ import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
+/**
+ * A panel which the children is vertically-centered. A decorator.
+ * 
+ * @author dtinth
+ */
 public class VerticallyCenteredPanel extends JPanel {
 
 	/** */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructs a new VerticallyCenteredPanel
+	 * 
+	 * @param component
+	 *            the component to wrap
+	 */
 	public VerticallyCenteredPanel(JComponent component) {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		component.setAlignmentX(0.5f);
